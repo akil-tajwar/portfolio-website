78787cf61a0d5656399f1bc0a55cc50aab5041ce
Akil Tajwar <akiltajwar@gmail.com>
2024-01-07 23:19:29 +0600

    adding portfolio animation

--- src/Container/Components/Banner.jsx
+++ src/Container/Components/Banner.jsx
@@ -32,7 +32,7 @@ const Banner = () => {
                         <div>
                             <p className='lg:w-1/2 md:w-2/3 border-l-4 mb-3 mt-4 pl-5 primary-border-color flex'>I'm a fullstack web developer with expertise in ReactJS, MongoDB, Express, and NodeJS. Along with my development skills, I also have a strong knowledge of competitive programming.</p>
                         </div>
-                        <button onClick={downloadResume} className='border-2 border-[#008f40] p-1 rounded mt-2'>
+                        <button onClick={downloadResume} className='border-2 border-[#008f40] p-1 drop-shadow-lg rounded mt-2'>
                             <div className='bg-[#008f40] rounded font-semibold text-lg p-2'>Download Resume</div>
                         </button>
                         {/* <button onClick={downloadResume} className='mt-3 bg-[#008f40] rounded font-semibold text-lg px-3 py-2'><span>Download Resume</span></button> */}
