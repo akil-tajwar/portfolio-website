dd6c4525bea3643d973e6b4a83674c699be4a7e4
Akil Tajwar <akiltajwar@gmail.com>
2024-01-08 00:05:03 +0600

    banner button animation

--- src/Container/Components/Banner.jsx
+++ src/Container/Components/Banner.jsx
@@ -33,7 +33,7 @@ const Banner = () => {
                             <p className='lg:w-1/2 md:w-2/3 border-l-4 mb-3 mt-4 pl-5 primary-border-color flex'>I'm a fullstack web developer with expertise in ReactJS, MongoDB, Express, and NodeJS. Along with my development skills, I also have a strong knowledge of competitive programming.</p>
                         </div>
                         <button onClick={downloadResume} className='border-2 border-[#008f40] p-1 drop-shadow-lg rounded mt-2'>
-                            <div className='bg-[#008f40] rounded font-semibold text-lg p-2'>Download Resume</div>
+                            <div className='bg-[#008f40] banner-button rounded font-semibold text-xl tracking-widest p-2'>Download CV</div>
                         </button>
                         {/* <button onClick={downloadResume} className='mt-3 bg-[#008f40] rounded font-semibold text-lg px-3 py-2'><span>Download Resume</span></button> */}
                     </div>
